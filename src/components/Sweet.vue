51f9e5c6bd48584059cba27981252b4820bd4d43
jared edwards <jredwards519@gmail.com>
2017-03-23 13:33:21 -0400

    initial commit

---
+++ src/components/Sweet.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="sweet">
+  When you have a great story about how your product or service was built to change lives, share it. The "About Us" page is a great place for it to live, too. Good stories humanize your brand, providing context and meaning for your product. What’s more, good stories are sticky -- which means people are more likely to connect with them and pass them on.
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sweet'
+}
+</script>
+<!-- styling for the component -->
+<style>
+#sweet {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
